7f639fa5aadcdcb173c5055ea1507aa454a1963b
mtea016 <33720308+mtea016@users.noreply.github.com>
2017-11-27 01:25:04 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+
 var clear = document.getElementById('clear');
 var contactBtn = document.getElementById('contact-btn');
+var projectBtn = document.getElementById('project-btn');
+var textHeader = document.getElementById('text-header');
+var textHeaderContainer = document.getElementById('text-header-container');
 var modalContainer = document.getElementById('modal-container');
 var body = document.getElementById('body');
+
 
 function closeModal() {
   modalContainer.className = 'connect-modal-container no-display';
@@ -9,10 +14,12 @@ function closeModal() {
 }
 
 contactBtn.onclick = function() {
+
   if(modalContainer.className = "no-display") {
     modalContainer.className = 'connect-modal-container display';
     modalContainer.style.opacity = '1';
   }
+
 }
 
 clear.style.display = 'none';
